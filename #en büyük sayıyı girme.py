e557d2f9cd233844594b33db916ed34ef255a705
Sweezy0 <119443953+Sweezy0@users.noreply.github.com>
2023-09-04 15:21:48 +0300

    Add files via upload

---
+++ #en büyük sayıyı girme.py
@@ -0,0 +1,6 @@
+#en büyük sayıyı girme
+s1=float(input("birinci sayıyı girin:"))
+s2=float(input("ikini sayıyı girin:"))
+s3=float(input("üçüncü sayıyı girin:"))
+en_buyuk_sayi=max(s1,s2,s3)
+print("en büyük sayı:",en_buyuk_sayi)
